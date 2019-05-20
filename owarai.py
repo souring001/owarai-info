e103c4764137d2c4dfa5d8179d4927dd188510af
aso <titechei@gmail.com>
2019-05-20 11:52:56 +0900

    filter today

--- owarai.py
+++ owarai.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib.request as req
 import re
+import datetime
 
 # HTMLを取得
 url = 'https://tv.so-net.ne.jp/s/comedy'
@@ -9,11 +10,34 @@ html = req.urlopen(url)
 # HTMLを解析する
 soup = BeautifulSoup(html, 'html.parser')
 
+# 今日の情報をリストにしたもの
+infoList = []
+
 # 必要な部分をCSSクエリで取り出す
-list = soup.select('div.utileList')
-for li in list:
+utileList = soup.select('div.utileList')
+for li in utileList:
+    info = ''
+
     title = li.select_one('h2 > a').string
-    print(title)
+    info += title + '\n'
+
     p = li.select_one('p:nth-of-type(1)')
     schedule = str(p).replace('<p class=\"utileListProperty\">', '').replace('<a href=\"/schedulesBySearch.action?condition.genres[0].id=105000&amp;stationPlatformId=0\">バラエティー</a>', '').replace('</p>', '').replace('\r\n', '').replace('\n', '').replace('          ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
-    print(schedule, '\n\n')
+    info += schedule
+
+    date = schedule.split()[0]
+    today = datetime.date.today()
+    m = today.month
+    d = today.day
+    str_today = str(m) + '/' + str(d)
+
+    # 今日の情報のみ追加
+    if date == str_today:
+        infoList.append(info)
+
+# 出力
+for li in infoList:
+    if(li == infoList[-1]):
+        print(li)
+    else:
+        print(li, '\n\n')
