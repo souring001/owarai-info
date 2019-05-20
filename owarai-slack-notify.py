3ab6aeed60be50ed9b37d79a7f9eee1744f076f7
aso <titechei@gmail.com>
2019-05-20 12:34:51 +0900

    owarai module

--- owarai-slack-notify.py
+++ owarai-slack-notify.py
@@ -1,29 +1,18 @@
-from bs4 import BeautifulSoup
-import urllib.request as req
-import re
+import owarai as ow
 import slackweb
 
 # SlackのIncoming Webhook URLを指定
 slack_url = 'https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
-# HTMLを取得
-url = 'https://tv.so-net.ne.jp/s/comedy'
-html = req.urlopen(url)
-
-# HTMLを解析する
-soup = BeautifulSoup(html, 'html.parser')
-
 # Slackで送信する内容をtextとする
 text = ''
 
-# 必要な部分をCSSクエリで取り出す
-list = soup.select('div.utileList')
-for li in list:
-    title = li.select_one('h2 > a').string
-    text += title + '\n'
-    p = li.select_one('p:nth-of-type(1)')
-    schedule = str(p).replace('<p class=\"utileListProperty\">', '').replace('<a href=\"/schedulesBySearch.action?condition.genres[0].id=105000&amp;stationPlatformId=0\">バラエティー</a>', '').replace('</p>', '').replace('\r\n', '').replace('\n', '').replace('          ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
-    text += schedule + '\n\n'
+owarai_list = ow.get_owarai_list()
+for li in owarai_list:
+    if(li == owarai_list[-1]):
+        text += li
+    else:
+        text += li + '\n\n'
 
 slack = slackweb.Slack(url=slack_url)
 slack.notify(text=text, username='owarai-info')
--- owarai.py
+++ owarai.py
@@ -3,41 +3,48 @@ import urllib.request as req
 import re
 import datetime
 
-# HTMLを取得
 url = 'https://tv.so-net.ne.jp/s/comedy'
-html = req.urlopen(url)
 
-# HTMLを解析する
-soup = BeautifulSoup(html, 'html.parser')
+def get_owarai_list():
 
-# 今日の情報をリストにしたもの
-infoList = []
+    # HTMLを取得
+    html = req.urlopen(url)
 
-# 必要な部分をCSSクエリで取り出す
-utileList = soup.select('div.utileList')
-for li in utileList:
-    info = ''
+    # HTMLを解析する
+    soup = BeautifulSoup(html, 'html.parser')
 
-    title = li.select_one('h2 > a').string
-    info += title + '\n'
+    # 今日の情報をリストにしたもの
+    infoList = []
 
-    p = li.select_one('p:nth-of-type(1)')
-    schedule = str(p).replace('<p class=\"utileListProperty\">', '').replace('<a href=\"/schedulesBySearch.action?condition.genres[0].id=105000&amp;stationPlatformId=0\">バラエティー</a>', '').replace('</p>', '').replace('\r\n', '').replace('\n', '').replace('          ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
-    info += schedule
+    # 必要な部分をCSSクエリで取り出す
+    utileList = soup.select('div.utileList')
+    for li in utileList:
+        info = ''
 
-    date = schedule.split()[0]
-    today = datetime.date.today()
-    m = today.month
-    d = today.day
-    str_today = str(m) + '/' + str(d)
+        title = li.select_one('h2 > a').string
+        info += title + '\n'
 
-    # 今日の情報のみ追加
-    if date == str_today:
-        infoList.append(info)
+        p = li.select_one('p:nth-of-type(1)')
+        schedule = str(p).replace('<p class=\"utileListProperty\">', '').replace('<a href=\"/schedulesBySearch.action?condition.genres[0].id=105000&amp;stationPlatformId=0\">バラエティー</a>', '').replace('</p>', '').replace('\r\n', '').replace('\n', '').replace('          ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
+        info += schedule
+
+        date = schedule.split()[0]
+        today = datetime.date.today()
+        m = today.month
+        d = today.day
+        str_today = str(m) + '/' + str(d)
+
+        # 今日の情報のみ追加
+        if date == str_today:
+            infoList.append(info)
+
+    return infoList
 
 # 出力
-for li in infoList:
-    if(li == infoList[-1]):
-        print(li)
-    else:
-        print(li, '\n\n')
+if __name__ == "__main__":
+    owarai_list = get_owarai_list()
+    for li in owarai_list:
+        if(li == owarai_list[-1]):
+            print(li)
+        else:
+            print(li, '\n\n')
